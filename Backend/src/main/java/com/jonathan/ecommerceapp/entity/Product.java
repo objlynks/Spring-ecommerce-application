1c6fc9a940bc1ecb41a310b2253af027ada16bbe
objlynks <obedjonathan26@gmail.com>
2023-03-25 17:37:46 +0100

    Developing JPA entities

---
+++ Backend/src/main/java/com/jonathan/ecommerceapp/entity/Product.java
@@ -0,0 +1,23 @@
+package com.jonathan.ecommerceapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    private Long id;
+    private String sku;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+}
