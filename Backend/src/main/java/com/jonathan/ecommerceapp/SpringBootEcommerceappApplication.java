f668ee4717417494a870c5bffd48daf1bf4c71f8
objlynks <obedjonathan26@gmail.com>
2023-03-25 17:24:20 +0100

    Back-end Initialization & Setup

---
+++ Backend/src/main/java/com/jonathan/ecommerceapp/SpringBootEcommerceappApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceappApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/jonathan/ecommerceapp/SpringBootEcommerceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
