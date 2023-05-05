68e37d56ae15b4da017c409b0fbc108b7c787f29
objlynks <obedjonathan26@gmail.com>
2023-05-05 12:22:59 +0100

    updated Product service and Product List component

--- angular-ecommerce/src/app/common/product.ts
+++ angular-ecommerce/src/app/common/product.ts
@@ -1,2 +1,16 @@
+// Ts product that matches all the actual properties of product coming from the Spring-boot app
 export class Product {
+    constructor(
+        public sku: string,
+        public name: string,
+        public description: string,
+        public unitPrice: number,
+        public imageUrl: string,
+        public active: boolean,
+        public unitsInStock: number,
+        public dateCreated: Date,
+        public lastUpdated: Date 
+    ){
+
+    }
 }
