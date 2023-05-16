5f9de3a523129e4ac096680eff66b6a3ef5dc381
objlynks <obedjonathan26@gmail.com>
2023-05-16 11:35:54 +0100

    Updated JPA entity for Product

--- Backend/src/main/java/com/jonathan/ecommerceapp/entity/Product.java
+++ Backend/src/main/java/com/jonathan/ecommerceapp/entity/Product.java
@@ -23,6 +23,9 @@ public class Product {
 
     @Column(name = "sku")
     private String sku;
+
+    @Column(name = "name")
+    private String name;
     @Column(name = "description")
     private String description;
     @Column(name = "unit_price")
